5de56d971ec1c6bd126d1b29a0061b2d06518826
Halit Aydin <halitaydin.tr@gmail.com>
2023-08-31 20:47:51 +0300

    Delete cached project-wordle

---
+++ project-wordle/src/components/GuessResult/GuessResult.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function GuessResult({ result, guesses, answer }) {
+  return (
+    <>
+      {result && (
+        <div className="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in{" "}
+            <strong>{guesses.length} guesses</strong>.
+          </p>
+        </div>
+      )}
+      {!result && result !== null && (
+        <div className="sad banner">
+          <p>
+            Sorry, the correct answer is <strong>{answer}</strong>.
+          </p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default GuessResult;
