970f18d5f6d7dc0a8ad4a1ca16a1e1c73c8dceac
Halit Aydin <halitaydin.tr@gmail.com>
2023-09-13 10:58:24 +0300

    Refactor ternary logic

--- project-wordle/src/components/Guess/Guess.js
+++ project-wordle/src/components/Guess/Guess.js
@@ -2,24 +2,17 @@ import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { checkGuess } from "../../game-helpers";
 
-function Guess({ guesses, answer, keyGuesses, counter }) {
+function Guess({ guesses, answer, keyGuesses, counter, num }) {
   return (
     <>
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <p key={num} className="guess">
-          {range(5).map((n) =>
-            guesses[num] ? (
-              <span key={n} className={`cell ${checkGuess(guesses[num], answer)[n].status}`}>
-                {guesses[num][n]}
-              </span>
-            ) : keyGuesses[n] ? (
-              <span key={n} className={`cell`}>
-                {keyGuesses[n][num - counter]}
-              </span>
-            ) : (
-              <span key={n} className="cell"></span>
-            )
-          )}
+        <p className="guess">
+          {range(5).map((n) => (
+            <span key={n} className={`cell ${guesses[num] && checkGuess(guesses[num], answer)[n].status}`}>
+              {guesses[num] ? guesses[num][n] : undefined}
+              {keyGuesses[n] ? keyGuesses[n][num - counter] : undefined}
+            </span>
+          ))}
         </p>
       ))}
     </>
