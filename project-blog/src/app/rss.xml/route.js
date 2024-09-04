183f974fe5716084c3233d7d938f882921d8ac4a
adel <halitaydin.tr@gmail.com>
2024-09-04 11:58:10 +0300

    Cloudflare migration

--- project-blog/src/app/rss.xml/route.js
+++ project-blog/src/app/rss.xml/route.js
@@ -1,34 +1,34 @@
-// import RSS from "rss";
-// import { getBlogPostList } from "@/helpers/file-helpers";
+import RSS from "rss";
+import { getBlogPostList } from "@/helpers/file-helpers";
 
-// export async function GET() {
-//   const site_url = "localhost:3000";
+export async function GET() {
+  const site_url = "localhost:3000";
 
-//   const feedOptions = {
-//     title: "Halit Aydin's Blog",
-//     description: "Focus on React, CSS, animation, and careers.",
-//     site_url,
-//     copyright: `All rights reserved ${new Date().getFullYear()}`,
-//   };
+  const feedOptions = {
+    title: "Halit Aydin's Blog",
+    description: "Focus on React, CSS, animation, and careers.",
+    site_url,
+    copyright: `All rights reserved ${new Date().getFullYear()}`,
+  };
 
-//   const feed = new RSS(feedOptions);
+  const feed = new RSS(feedOptions);
 
-//   const posts = await getBlogPostList();
+  const posts = await getBlogPostList();
 
-//   // Add each individual post to the feed.
-//   posts.map((post) => {
-//     feed.item({
-//       title: post.title,
-//       description: post.abstract,
-//       url: `${site_url}/${post.slug}`,
-//       date: post.publishedOn,
-//       author: "Halit Aydin",
-//     });
-//   });
+  // Add each individual post to the feed.
+  posts.map((post) => {
+    feed.item({
+      title: post.title,
+      description: post.abstract,
+      url: `${site_url}/${post.slug}`,
+      date: post.publishedOn,
+      author: "Halit Aydin",
+    });
+  });
 
-//   return new Response(feed.xml({ indent: true }), {
-//     headers: {
-//       "Content-Type": "application/atom+xml; charset=utf-8",
-//     },
-//   });
-// }
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      "Content-Type": "application/atom+xml; charset=utf-8",
+    },
+  });
+}
